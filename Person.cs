1da37cc1988c2e8802e6c22142998d1d5e7d6d57
naim90 <naim.elmoudni@gmail.com>
2021-11-23 08:59:32 +0100

    composite

---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Person
+    {
+
+        public string Prenom { get; set; }
+        public List<Person> Descendants { get; set; }
+
+       public Person(string Prenom)
+        {
+            this.Prenom = Prenom;
+            this.Descendants = new List<Person>();
+        }
+
+        public override string ToString()
+        {
+                       string PrenomPersonne;
+                        foreach(var personne in Descendants)
+                        {
+                            PrenomPersonne=personne.Prenom;
+                        }
+
+            return Descendants.ToString();
+        }
+ 
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Moi = new Person("Jeff");
+            Person Frere = new Person("Naim");
+            Person Soeur = new Person("Laura");
+            Person Cousin1= new Person("Jean");
+            Person Cousin2 = new Person("Bob");
+
+            Moi.Descendants = new List<Person>() { Frere,Soeur,Cousin1,Cousin2};
+
+            Console.WriteLine(Moi.ToString());
+
+            Console.Write("_");
+
+
+        }
+    }
+}
